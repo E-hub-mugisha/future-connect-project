52d9af640fea2408a43082e338d4684c609ee02c
mugisha erik <54761531+E-hub-mugisha@users.noreply.github.com>
2025-07-21 06:03:24 +0200

    last retry

--- .cpanel-ci.js
+++ .cpanel-ci.js
@@ -5,9 +5,9 @@ import { deploy, excludeDefaults } from "@samkirkland/ftp-deploy";
 async function deployToCPanel() {
   console.log("🚚 Deploy started");
   await deploy({
-    server: process.env.FTP_SERVER,
-    username: process.env.FTP_USERNAME,
-    password: process.env.FTP_PASSWORD,
+    server: "ftp.futureconnect.rw",
+    username: "futureconnect",
+    password: "AltisCorolla@2008",
     "server-dir": "/website/",
     exclude: [...excludeDefaults, ".env", ".git/**", "node_modules/**", "vendor/**"],
   });
